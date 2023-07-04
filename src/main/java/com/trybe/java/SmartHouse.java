bfc32e62b199c27847620b4e795c6e7789fa4ed1
Pablo Dominguez <pasaxo@gmail.com>
2023-07-04 12:01:20 -0300

    requisito 5

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -1,14 +1,28 @@
 package com.trybe.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class Smart House.
  **/
 
 public class SmartHouse {
   private boolean lampada;
+  private Map<String, String> estados = new HashMap<>();
+
+  /**
+   * SmarthHouse class constructor.
+   * 
+   * @param lampada Initial state of the lamp (on/off).
+   */
+
 
   public SmartHouse(boolean lampada) {
     this.lampada = false;
+    this.estados.put("PE", "Pernambuco");
+    this.estados.put("SC", "Santa Catarina");
+    this.estados.put("AM", "Amazonas");
   }
 
   /**
@@ -53,6 +67,22 @@ public class SmartHouse {
       }
     }
     return false;
+  }
+
+  /**
+   * Returns the full name of a state from the abbreviation.
+   * 
+   * @param estado The abbreviation of a state.
+   * @return The full name of the state or "Estado não cadastrado" if the abbreviation is not
+   *         registered.
+   */
+
+  public String mostraNomeEstado(String estado) {
+    String valueEstado = this.estados.get(estado);
+    if (valueEstado != null) {
+      return valueEstado;
+    }
+    return "Estado não cadastrado";
   }
 
 }
