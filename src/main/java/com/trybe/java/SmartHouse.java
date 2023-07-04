c16bd5837d209ae346139801d5971919c163bd95
Pablo Dominguez <pasaxo@gmail.com>
2023-07-04 09:51:51 -0300

    Solucionando problemas de Checkstyle

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -11,6 +11,10 @@ public class SmartHouse {
     this.lampada = false;
   }
 
+  /**
+   * Liga lâmpada, se estiver desligada.
+   */
+
   public void ligarLampada() {
     if (this.lampada == false) {
       this.lampada = true;
@@ -19,6 +23,10 @@ public class SmartHouse {
     }
   }
 
+  /**
+   * Desliga a lâmpada se estiver ligada.
+   */
+
   public void desligarLampada() {
     if (this.lampada == true) {
       this.lampada = false;
@@ -26,6 +34,12 @@ public class SmartHouse {
       System.out.println("A lampada já está ligada");
     }
   }
+
+  /**
+   * Verifica a situação da lâmpada se está ligada ou desligada.
+   * 
+   * @return true se estiver ligada, false se estiver desligada.
+   */
 
   public boolean isLampadaLigada() {
     return this.lampada;
