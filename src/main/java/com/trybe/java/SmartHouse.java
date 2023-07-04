8ef031ae7345ebc6ba1ddb99f8e9e7d6529871ed
Pablo Dominguez <pasaxo@gmail.com>
2023-07-04 09:41:46 -0300

    Requisito 1

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -1,11 +1,35 @@
 package com.trybe.java;
 
 /**
- * Classe.
+ * Class .
  **/
 
 public class SmartHouse {
+  private boolean lampada;
 
-  
+  public SmartHouse() {
+    this.lampada = false;
+  }
+
+  public void ligarLampada() {
+    if (this.lampada == false) {
+      this.lampada = true;
+    } else {
+      System.out.println("A lampada já está desligada");
+    }
+  }
+
+  public void desligarLampada() {
+    if (this.lampada == true) {
+      this.lampada = false;
+    } else {
+      System.out.println("A lampada já está ligada");
+    }
+  }
+
+  public boolean isLampadaLigada() {
+    return this.lampada;
+  }
+
 }
 
